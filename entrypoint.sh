0cdf13ac6f3cdee4db80e0ef2a9fb675d67e8c7f
Pradeep Kumar A V <pradeepkumar1025@gmail.com>
2022-02-15 16:08:28 +0000

    Initial patch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -v KUBECONFIG ]]; then
-KUBECONFIG_ARG="--kubeconfig=$KUBECONFIG"
+    KUBECONFIG_ARG="--kubeconfig=$KUBECONFIG"
+fi
 
 /kube-explorer $KUBECONFIG_ARG --http-listen-port=8080 --https-listen-port=0
